938dc49a1d8562cb1102945f45012bba102cac9b
Dhruv Sahu <dhruvsahu797@gmail.com>
2021-11-07 12:55:39 +0530

    First release of Hello World!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React,{useState,useEffect} from 'react';
 import './App.css';
 
+const quotes = [
+  "“Life is what happens when you’re busy making other plans.”",
+  "“Get busy living or get busy dying.”",
+  "“You only live once, but if you do it right, once is enough.”",
+  "“Many of life’s failures are people who did not realize how close they were to success when they gave up.”",
+  "“Never let the fear of striking out keep you from playing the game.”",
+  "“Money and success don’t change people; they merely amplify what is already there.”"
+]
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [quote,setQuote] = useState(quotes[0]);
+
+  function randomizeQuote(){
+    const randomQuote = quotes[Math.floor(Math.random() * quotes.length)]
+    setQuote(randomQuote);
+  }
+
+  return <div className="App">
+      <h4>{quote}</h4>
+      <button onClick={randomizeQuote}>Click Me</button>
+  </div>
 }
 
 export default App;
